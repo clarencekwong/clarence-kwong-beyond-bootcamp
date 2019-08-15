788d9a2b8dd1a4fb90dbd4d12f17de8d8d4b50dd
github username <clarencekwong7@gmail.com>
2019-08-15 14:57:03 -0400

    minor change on silhouette function

--- src/game.js
+++ src/game.js
@@ -44,14 +44,9 @@ document.addEventListener("DOMContentLoaded", () => {
 			if (isSilhouette) {
 				let rawImage = ctx.getImageData(0, 0, canvas.width, canvas.height);
 				for (var i = 0; i < rawImage.data.length; i += 4) {
-					if (rawImage.data[i + 3] >= 100) {
-						rawImage.data[i] = 30;
-						rawImage.data[i + 1] = 30;
-						rawImage.data[i + 2] = 30;
-						rawImage.data[i + 3] = 255;
-					} else {
-						rawImage.data[i + 3] = 0;
-					}
+					rawImage.data[i] = 30;
+					rawImage.data[i + 1] = 30;
+					rawImage.data[i + 2] = 30;
 				}
 
 				ctx.putImageData(rawImage, 0, 0);
